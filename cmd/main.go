045639414701e6f27bcfc49503bf636ab4ead743
dan-lay <66338879+dan-lay@users.noreply.github.com>
2024-02-07 18:48:29 -0800

    Revamp 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"test-go-app/handler"
-
-	"github.com/labstack/echo/v4"
-)
-
-type DB struct{}
-
 func main() {
-	app := echo.New()
-
-	userHandler := handler.UserHandler{}
-	app.GET("/user", userHandler.HandleUserShow)
-	app.Start(":3000")
-
-	fmt.Println("hello it's working")
+	
 }
